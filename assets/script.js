2b1a89d3ca9e0a180bef093a3a0d16ebccbe65ee
E Alexander <erick.interiano05@gmail.com>
2021-02-23 19:20:28 -0800

    Created functions to update time and block out the hour rows once the respective time has passed

--- assets/script.js
+++ assets/script.js
@@ -8,5 +8,25 @@ $(document).ready(function() {
         localStorage(time, value);
     });
 
-    
+    function hourUpdater() {
+
+        var currentHour = moment().hours();
+
+        $(".time-block").each(function() {
+            var blockHour = pareInt($(this).attr("id").split("-")[1]);
+
+            if ( blockHour < currentHour) {
+                $(this).addClass("past");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    }
 })
